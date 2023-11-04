7bde65e8a4a3c08169a6e6280a134736b55b3d0d
Kat Saltzman <katherinesaltzman70@gmail.com>
2023-11-04 16:28:32 -0400

    Add files via upload

---
+++ splay.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+import json
+from typing import List
+
+verbose = False
+
+# DO NOT MODIFY!
+class Node():
+    def  __init__(self,
+                  key       : int,
+                  leftchild  = None,
+                  rightchild = None,
+                  parent     = None,):
+        self.key        = key
+        self.leftchild  = leftchild
+        self.rightchild = rightchild
+        self.parent     = parent
+
+# DO NOT MODIFY!
+class SplayTree():
+    def  __init__(self,
+                  root : Node = None):
+        self.root = root
+
+    # For the tree rooted at root:
+    # Return the json.dumps of the object with indent=2.
+    # DO NOT MODIFY!
+    def dump(self) -> str:
+        def _to_dict(node) -> dict:
+            pk = None
+            if node.parent is not None:
+                pk = node.parent.key
+            return {
+                "key": node.key,
+                "left": (_to_dict(node.leftchild) if node.leftchild is not None else None),
+                "right": (_to_dict(node.rightchild) if node.rightchild is not None else None),
+                "parentkey": pk
+            }
+        if self.root == None:
+            dict_repr = {}
+        else:
+            dict_repr = _to_dict(self.root)
+        return json.dumps(dict_repr,indent = 2)
+
+    # Search
+    def search(self,key:int):
+        print('This is a place-holder')
+
+    # Insert Method 1
+    def insert(self,key:int):
+        print('This is a place-holder')
+
+    # Delete Method 1
+    def delete(self,key:int):
+        print('This is a place-holder')
