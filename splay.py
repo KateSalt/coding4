65b7c1666921cf01c5d6b557b3df0db6133c34ee
KateSaltz <katherinesaltzman70@gmail.com>
2023-11-08 16:23:48 -0500

    ymca rats

--- splay.py
+++ splay.py
@@ -56,41 +56,118 @@ class SplayTree():
                     curr = curr.leftchild
                 else:
                     return curr
-    # Search
+    # Search add until root
     def search(self,key:int):
+        if(self.root == None):
+            
+            return self
         splaykey = self.searchhelp(key)
         if(splaykey.parent == None):
-            print("this is the root")
-        elif(splaykey.parent.parent == None):
-            print("this is a child of the root")
-            if(splaykey == splaykey.parent.rightchild):
-                print("this is a zig right")
-            else:
-                print("this is a zig left")
-        elif(splaykey.parent == splaykey.parent.parent.rightchild):
-            if(splaykey == splaykey.parent.rightchild):
-                print("this is a zig zig for the right")
-            else:
-                print("this is a zig zag -right to left")
-        elif(splaykey.parent == splaykey.parent.parent.leftchild):
-            if(splaykey == splaykey.parent.leftchild):
-                print("this is a zig zig for left")
-            else:
-                print("this is zig zag - left to right")
+            return self
+        while(splaykey.parent != None):
+            if(splaykey.parent.parent == None):
+                if(splaykey == splaykey.parent.rightchild):
+                    self.root = splaykey
+                    splaykey.parent.rightchild = splaykey.leftchild
+                    splaykey.leftchild = splaykey.parent
+                    splaykey.parent.parent = splaykey 
+                    splaykey.parent = None
+                    return self
+                else:
+                    self.root = splaykey
+                    splaykey.parent.leftchild = splaykey.leftchild
+                    splaykey.rightchild = splaykey.parent
+                    splaykey.parent.parent = splaykey 
+                    splaykey.parent = None
+                    return self
+            elif(splaykey.parent == splaykey.parent.parent.rightchild):
+                if(splaykey == splaykey.parent.rightchild):
+                    grand = splaykey.parent.parent
+                    parent = splaykey.parent
+                    if(grand == self.root):
+                        self.root = splaykey
+                    grand.rightchild = parent.leftchild
+                    parent.leftchild = grand
+                    splaykey.parent = grand.parent 
+                    if(grand.parent != None):
+                        if(grand.parent.rightchild == grand):
+                            grand.parent.rightchild = splaykey
+                        else:
+                            grand.parent.leftchild = splaykey
+                    grand.parent = parent 
+                    parent.parent = splaykey
+                    if(splaykey.leftchild != None):
+                        splaykey.leftchild.parent = parent
+
+                    parent.rightchild = splaykey.leftchild
+                    splaykey.leftchild = parent
+                else:
+                    grand = splaykey.parent.parent
+                    parent = splaykey.parent
+                    parent.parent = splaykey 
+                    parent.rightchild = splaykey.leftchild
+                    splaykey.leftchild = parent 
+                    splaykey.parent = grand.parent 
+                    grand.leftchild = splaykey.rightchild
+                    grand.parent = splaykey
+
+            elif(splaykey.parent == splaykey.parent.parent.leftchild):
+                if(splaykey == splaykey.parent.leftchild):
+                    grand = splaykey.parent.parent
+                    parent = splaykey.parent
+                    if(grand == self.root):
+                        self.root = splaykey
+                    grand.leftchild = parent.rightchild
+                    parent.rightchild = grand
+                    splaykey.parent = grand.parent 
+                    if(grand.parent != None):
+                        if(grand.parent.leftchild == grand):
+                            grand.parent.leftchild = splaykey
+                        else:
+                            grand.parent.rightchild = splaykey
+                    grand.parent = parent 
+                    parent.parent = splaykey
+                    if(splaykey.rightchild != None):
+                        splaykey.rightchild.parent = parent
+
+                    parent.leftchild = splaykey.rightchild
+                    splaykey.rightchild = parent
+
+                else:
+                    grand = splaykey.parent.parent
+                    parent = splaykey.parent
+                    parent.parent = splaykey 
+                    parent.leftchild = splaykey.rightchild
+                    grand.rightchild = splaykey.leftchild
+                    splaykey.rightchild = parent 
+                    splaykey.parent = grand.parent 
+                    grand.parent = splaykey
+        return self
 
     # Insert Method 1
     def insert(self,key:int):
+        if(self.root == None):
+            self.root = Node(key=key,rightchild=None,leftchild=None,parent=None)
+            return self
         self = self.search(key)
-        if(self.root < key):
+        if(self.root.key < key):
             curr = self.root
-            curr.parent = Node(key=key,leftchild=curr,rightchild=curr.rightchild, parent= None)
-            self.root = curr.parent
-            curr.rightchild.parent = self.root
+            new = Node(key=key,leftchild=curr,rightchild=curr.rightchild, parent= None)
+            self.root = new
+            curr.parent = new
+            if(curr.rightchild != None):
+                curr.rightchild.parent = new
+            curr.rightchild = None
+            return self
         else:
             curr = self.root
-            curr.parent = Node(key=key,leftchild=curr.leftchild,rightchild=curr, parent= None)
-            self.root = curr.parent
-            curr.leftchild.parent = self.root
+            new = Node(key=key,leftchild=curr.leftchild,rightchild=curr, parent= None)
+            self.root = new
+            curr.parent = new
+            if(curr.leftchild != None):
+                curr.leftchild.parent = new
+            curr.leftchild = None
+            return self
 
     # Delete Method 1
     def delete(self,key:int):
@@ -108,4 +185,7 @@ class SplayTree():
             splaykey.rightchild.parent = replacement
             splaykey.leftchild.parent = replacement
             self.root = replacement 
-        print('This is a place-holder')
+            return replacement
+        
+
+
